54ae069795755c99c45ed02c85a68b32b5e7340b
Necmettin Kizildag <necmettin_kzldg@hotmailcom>
2023-10-10 11:15:22 +0200

    day11 C04 added

---
+++ src/test/java/day11_fileTestleri_Waits/C05_ExplicitWait.java
@@ -0,0 +1,20 @@
+package day11_fileTestleri_Waits;
+
+import org.junit.Test;
+
+public class C05_ExplicitWait {
+
+    @Test
+    public  void test01(){
+        // webdriver ayarlarini implicitly wait olmadan yapin
+        //2. Iki tane metod olusturun : implicitWait() , explicitWait()
+        //Iki metod icin de asagidaki adimlari test edin.
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        //4. Remove butonuna basin.
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        //6. Add buttonuna basin
+        //7. It’s back mesajinin gorundugunu test edin
+
+
+    }
+}
