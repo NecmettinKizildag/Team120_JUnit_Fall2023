95ea4c03ec64ca21c35aa34c9599b1aa7a4f6e30
Necmettin Kizildag <necmettin_kzldg@hotmailcom>
2023-10-10 15:47:34 +0200

    day11 C05 added

--- src/test/java/day11_fileTestleri_Waits/C05_ExplicitWait.java
+++ src/test/java/day11_fileTestleri_Waits/C05_ExplicitWait.java
@@ -1,20 +1,58 @@
 package day11_fileTestleri_Waits;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class C05_ExplicitWait {
 
     @Test
     public  void test01(){
         // webdriver ayarlarini implicitly wait olmadan yapin
-        //2. Iki tane metod olusturun : implicitWait() , explicitWait()
-        //Iki metod icin de asagidaki adimlari test edin.
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
         //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
         //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+
         //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+         /*
+            Gorunmeyen bir elementi locate edemeyiz
+            locate edilmeyen bir elementi bekleyemeyiz
+            gorunur olmasi icin de beklememiz lazim
+
+            Explicit Wait bekleme ve locate islemini birlikte yapabilir
+         */
+
+       //WebElement itsGoneText = driver.findElement(By.id("message"));
+
+        WebElement itsGoneText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+
+        Assert.assertTrue(itsGoneText.isDisplayed());
+
         //6. Add buttonuna basin
+        driver.findElement(By.xpath("//*[text()='Add']")).click();
+
         //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
 
+        Assert.assertTrue(itsBackText.isDisplayed());
 
+        driver.close();
     }
 }
